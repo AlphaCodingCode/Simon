978172d96d2c405291922164b0e4d9ef4e6313c0
AlphaCodingcode <dilshan@alphacoding.com>
2019-07-16 13:34:02 -0400

    simon game complete

--- sketch.js
+++ sketch.js
@@ -1,7 +1,13 @@
 // Any global variables can be defined up here
-let simonHistory = [];
+let simonHistory = [1, 0, 2, 3];
 let guesses = [];
 let lightupSquare = 4; // 0 - 3 valid, 4+ invalid
+let simonsTurn = true;
+
+let lightupDelay = 0;
+let currentSimonIndex = 0;
+let lightout = false;
+let lightoutTimer = 0;
 
 /*
     Code in the setup function will only be run once at the start of the animation
@@ -15,9 +21,78 @@ function setup() {
 */
 function draw() {
     // Update
+    // if it's a lights out turn wait 60 frames
+    if (lightout && lightupDelay == 0) {
+        lightoutTimer--;
+        lightupSquare = 4;
+        if (lightoutTimer == 0) {
+            lightout = false;
+        }
+    } else {
+        if (simonsTurn) {
+            guesses = [];
+            // if simon's history has an index, currentSimonIndex, light up that square for delay time
+            if (simonHistory.length - 1 >= currentSimonIndex && lightupDelay == 0) {
+                lightupSquare = simonHistory[currentSimonIndex];
+                lightupDelay = 45;
+                currentSimonIndex++;
+                lightoutTimer = 45;
+                lightout = true;
+            } else if (simonHistory.length - 1 < currentSimonIndex && lightupDelay == 0) {
+                // show the newly added square
+                simonHistory.push(round(random(0, 3)));
+                lightupSquare = simonHistory[simonHistory.length - 1];
+                currentSimonIndex = 0;
+                lightupDelay = 60;
+                simonsTurn = false;
+                lightoutTimer = 45;
+                lightout = true;
+            }
+
+        }
+        if (guesses.length > 0 && guesses.length == simonHistory.length) {
+            simonsTurn = true;
+            lightupDelay = 45;
+        }
+        lightupDelay = constrain(lightupDelay - 1, 0, 60);
+
+    }
+
     // Render
     drawBoard();
 }
+
+
+function mouseClicked() {
+    if (simonsTurn)
+        return;
+    // check which square was clicked, and add it to the guess array
+    if (mouseX <= width / 2 && mouseY <= height / 2) {
+        // 0
+        guesses.push(0);
+        lightupSquare = 0;
+    } else if (mouseX <= width && mouseY <= height / 2) {
+        // 1
+        guesses.push(1);
+        lightupSquare = 1;
+    } else if (mouseX <= width / 2 && mouseY >= height / 2) {
+        // 2
+        guesses.push(2);
+        lightupSquare = 2;
+    } else {
+        // 3
+        guesses.push(3);
+        lightupSquare = 3;
+    }
+    // check guesses so far match simonHistory
+    for (let i = 0; i < guesses.length; i++) {
+        if (guesses[i] != simonHistory[i]) {
+            // wrong guess
+        }
+    }
+    // guess was correct
+}
+
 
 function drawBoard() {
     // overlay board with dark rectangles for each
